abf97e9967cc612c05c918b793abbfba7e4802ef
James Rogers <jarogers095@gmail.com>
2020-01-26 10:26:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   if (array.size == 2)
     return array.join(" and ")
   elsif (array.size > 2)
+    array[-1] = "and #{array[-1]}"
     return array.join(", ")
   else
     return array[0]
